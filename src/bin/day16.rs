558568d5bb763156cc80ae982c27db195a25fd79
Stefan Seemayer <stefan@seemayer.de>
2019-12-30 20:14:10 +0100

    remove leftover test

--- src/bin/day16.rs
+++ src/bin/day16.rs
@@ -112,24 +112,3 @@ fn main() -> Result<()> {
 
     Ok(())
 }
-
-#[cfg(test)]
-mod test {
-    use super::*;
-
-    #[test]
-    fn test_recipe_scoring() -> Result<()> {
-        let butterscotch =
-            "Butterscotch: capacity -1, durability -2, flavor 6, texture 3, calories 8".parse()?;
-        let cinnamon =
-            "Cinnamon: capacity 2, durability 3, flavor -2, texture -1, calories 3".parse()?;
-
-        let mut recipe = HashMap::new();
-        recipe.insert(&butterscotch, 44);
-        recipe.insert(&cinnamon, 56);
-
-        assert_eq!(score(&recipe), (62842880, 520));
-
-        Ok(())
-    }
-}
